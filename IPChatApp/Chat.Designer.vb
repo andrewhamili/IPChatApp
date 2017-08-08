895c87d4a443c6670bfac7a211ab7607a9c8d20a
andrewhamili <rockr1521@gmail.com>
2017-08-08 09:46:16 +0800

    Add project files

---
+++ IPChatApp/Chat.Designer.vb
@@ -0,0 +1,204 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Chat
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtServer = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnConnect = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.rtbMessages = New System.Windows.Forms.RichTextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.rtbBody = New System.Windows.Forms.RichTextBox()
+        Me.btnSend = New System.Windows.Forms.Button()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.cmsExit = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.Exit1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.cmsExit.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(124, 26)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(100, 26)
+        Me.txtName.TabIndex = 0
+        '
+        'txtServer
+        '
+        Me.txtServer.Location = New System.Drawing.Point(320, 24)
+        Me.txtServer.Name = "txtServer"
+        Me.txtServer.Size = New System.Drawing.Size(126, 26)
+        Me.txtServer.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(106, 18)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Display Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(230, 29)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(84, 18)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "IP Address"
+        '
+        'btnConnect
+        '
+        Me.btnConnect.Location = New System.Drawing.Point(452, 24)
+        Me.btnConnect.Name = "btnConnect"
+        Me.btnConnect.Size = New System.Drawing.Size(101, 23)
+        Me.btnConnect.TabIndex = 4
+        Me.btnConnect.Text = "Connect"
+        Me.btnConnect.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.rtbMessages)
+        Me.GroupBox1.Location = New System.Drawing.Point(9, 287)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(527, 150)
+        Me.GroupBox1.TabIndex = 5
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Messages"
+        '
+        'rtbMessages
+        '
+        Me.rtbMessages.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.rtbMessages.Location = New System.Drawing.Point(3, 22)
+        Me.rtbMessages.Name = "rtbMessages"
+        Me.rtbMessages.ReadOnly = True
+        Me.rtbMessages.Size = New System.Drawing.Size(521, 125)
+        Me.rtbMessages.TabIndex = 0
+        Me.rtbMessages.Text = ""
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.rtbBody)
+        Me.GroupBox2.Location = New System.Drawing.Point(6, 70)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(417, 174)
+        Me.GroupBox2.TabIndex = 5
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Body"
+        '
+        'rtbBody
+        '
+        Me.rtbBody.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.rtbBody.Location = New System.Drawing.Point(3, 22)
+        Me.rtbBody.Name = "rtbBody"
+        Me.rtbBody.Size = New System.Drawing.Size(411, 149)
+        Me.rtbBody.TabIndex = 0
+        Me.rtbBody.Text = ""
+        '
+        'btnSend
+        '
+        Me.btnSend.Location = New System.Drawing.Point(441, 150)
+        Me.btnSend.Name = "btnSend"
+        Me.btnSend.Size = New System.Drawing.Size(101, 23)
+        Me.btnSend.TabIndex = 4
+        Me.btnSend.Text = "Send"
+        Me.btnSend.UseVisualStyleBackColor = True
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info
+        Me.NotifyIcon1.BalloonTipText = "New Message"
+        Me.NotifyIcon1.BalloonTipTitle = "New Message"
+        Me.NotifyIcon1.ContextMenuStrip = Me.cmsExit
+        Me.NotifyIcon1.Text = "Chat"
+        Me.NotifyIcon1.Visible = True
+        '
+        'cmsExit
+        '
+        Me.cmsExit.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.Exit1})
+        Me.cmsExit.Name = "cmsExit"
+        Me.cmsExit.Size = New System.Drawing.Size(93, 26)
+        '
+        'Exit1
+        '
+        Me.Exit1.Name = "Exit1"
+        Me.Exit1.Size = New System.Drawing.Size(92, 22)
+        Me.Exit1.Text = "Exit"
+        '
+        'Timer1
+        '
+        '
+        'Timer2
+        '
+        '
+        'Chat
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(554, 449)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btnSend)
+        Me.Controls.Add(Me.btnConnect)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtServer)
+        Me.Controls.Add(Me.txtName)
+        Me.Font = New System.Drawing.Font("Arial", 12.0!)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.MaximizeBox = False
+        Me.Name = "Chat"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Chat"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.cmsExit.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents txtServer As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnConnect As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents rtbMessages As RichTextBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents rtbBody As RichTextBox
+    Friend WithEvents btnSend As Button
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents cmsExit As ContextMenuStrip
+    Friend WithEvents Exit1 As ToolStripMenuItem
+    Friend WithEvents Timer2 As Timer
+End Class
---
+++ IPChatApp/Chat.vb
@@ -0,0 +1,147 @@
+﻿Imports System.IO
+Imports System.Net.Sockets
+Imports System.Threading
+
+Public Class Chat
+
+    Dim Listener As New TcpListener(65535)
+    Dim Client As New TcpClient
+    Dim Message As String = ""
+
+    Dim ListenerThread As New Thread(New ThreadStart(AddressOf Listening))
+
+    Private Sub Listening()
+        Listener.Start()
+    End Sub
+
+    Protected Overrides ReadOnly Property CreateParams() As CreateParams
+        Get
+            Dim Param As CreateParams = MyBase.CreateParams
+            Param.ClassStyle = Param.ClassStyle Or &H200
+            Return Param
+        End Get
+    End Property
+
+    Private Sub Chat_Load(sender As Object, e As EventArgs) Handles Me.Load
+        NotifyIcon1.Visible = True
+        NotifyIcon1.Icon = SystemIcons.Application
+        NotifyIcon1.BalloonTipIcon = ToolTipIcon.Info
+        Listening()
+        Timer1.Enabled = True
+    End Sub
+    Private Sub btnConnect_Click(sender As Object, e As EventArgs) Handles btnConnect.Click
+
+        If btnConnect.Text = "Connect" Then
+            Dim isOnline As Boolean = False
+            Try
+                If My.Computer.Network.Ping(txtServer.Text) Then
+                    isOnline = True
+                Else
+                    isOnline = False
+                End If
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+            If isOnline = True Then
+
+                txtName.Enabled = False
+                txtServer.Enabled = False
+                btnConnect.Text = "Disconnect"
+            Else
+                MsgBox("Cannot connect because the IP address is unreachable.")
+            End If
+        Else
+            txtName.Enabled = True
+            txtServer.Enabled = True
+            btnConnect.Text = "Connect"
+        End If
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        If Listener.Pending = True Then
+            Message = ""
+            Client = Listener.AcceptTcpClient()
+            Console.WriteLine("Fire1")
+
+            Dim Reader As New StreamReader(Client.GetStream())
+            While Reader.Peek > -1
+                Message = Message + Convert.ToChar(Reader.Read()).ToString
+
+                Console.WriteLine("Fire2")
+            End While
+            With rtbMessages
+                .ForeColor = Color.Black
+                .Text += Message + vbCrLf
+
+                NotifyIcon1.BalloonTipTitle = "New Message"
+                NotifyIcon1.BalloonTipText = "New Message"
+                NotifyIcon1.ShowBalloonTip(20000)
+
+                If Me.WindowState = FormWindowState.Normal Then
+                Else
+                    NotifyIcon1.Icon = SystemIcons.Information
+                End If
+
+
+
+            End With
+        End If
+    End Sub
+    Private Sub btnSend_Click(ByVal sender As System.Object,
+            ByVal e As System.EventArgs) Handles btnSend.Click
+
+        'Check to make sure that the user has entered 
+        'a display name, and a client IP Address
+        'If Not, Show a Message Box
+
+        Try
+            Client = New TcpClient(txtServer.Text, 65535)
+
+            'Declare the Client as an IP Address. 
+            'Must be in the Correct form. eg. 000.0.0.0
+            Dim Writer As New StreamWriter(Client.GetStream())
+
+            Writer.Write(txtName.Text & " Says:  " & rtbBody.Text & vbCrLf)
+            Writer.Flush()
+
+            'Write the Message in the stream
+
+            rtbMessages.Text += (txtName.Text & " Says:  " & rtbBody.Text & vbCrLf) + vbCrLf
+            rtbBody.Text = ""
+        Catch ex As Exception
+            Console.WriteLine(ex)
+            Dim Errorresult As String = ex.Message
+            MessageBox.Show(Errorresult & vbCrLf & vbCrLf &
+                                "Please Review Client Address",
+                                "Error Sending Message",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub NotifyIcon1_DoubleClick(sender As Object, e As EventArgs) Handles NotifyIcon1.DoubleClick, NotifyIcon1.BalloonTipClicked
+        If Me.WindowState = FormWindowState.Normal Then
+            Me.WindowState = FormWindowState.Minimized
+            Me.ShowInTaskbar = False
+        Else
+            Me.WindowState = FormWindowState.Normal
+            NotifyIcon1.Icon = SystemIcons.Application
+            Me.ShowInTaskbar = True
+        End If
+        Timer2.Enabled = False
+    End Sub
+    Private Sub Chat_Resize(sender As Object, e As EventArgs) Handles MyBase.Resize
+        If Me.WindowState = FormWindowState.Minimized Then
+            Me.ShowInTaskbar = False
+        Else
+            Me.ShowInTaskbar = True
+        End If
+    End Sub
+
+    Private Sub Exit1_Click(sender As Object, e As EventArgs) Handles Exit1.Click
+        ListenerThread.Abort()
+        Me.Close()
+    End Sub
+
+End Class
